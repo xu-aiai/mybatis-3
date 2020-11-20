b60898b5016637dae2f5f51f698b45efcb4995a7
xuaiai <974210695@qq.com>
2020-11-20 22:57:43 +0800

    Update ConnectDB.java
    添加创建 SqlSessionFactory 的注释

--- src/test/java/xuaiai/connection/ConnectDB.java
+++ src/test/java/xuaiai/connection/ConnectDB.java
@@ -12,6 +12,19 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
 
+/**
+ * 每个基于 MyBatis 的应用都是以一个 SqlSessionFactory 的实例为核心的。
+ *
+ * SqlSessionFactory 的实例可以通过 SqlSessionFactoryBuilder 获得。
+ * 而 SqlSessionFactoryBuilder 则可以从 XML 配置文件或
+ * 一个预先配置的 Configuration 实例来构建出 SqlSessionFactory 实例。
+ * 因此 sqlSessionFactory 有两种构建方式：
+ *
+ * ①使用xml创建 SqlSessionFactory ② 不使用 xml 创建SqlSessionFactory。
+ * @author: 啦儿啦
+ * @date: 20201119
+ * @description: 创建 SqlSessionFactory 的两种方法
+ */
 public class ConnectDB {
 
   /**
@@ -37,6 +50,11 @@ public class ConnectDB {
     return sqlSessionFactory;
   }
 
+  /**
+   * 使用 xml 创建 SqlSessionFactory
+   * @param resource: xml 文件路径
+   * @return: SqlSessionFactory 实例
+   */
   public SqlSessionFactory getSqlSessionFactory(String resource) throws IOException {
     InputStream inputStream = Resources.getResourceAsStream(resource);
     SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
